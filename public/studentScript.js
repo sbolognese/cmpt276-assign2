d5b1b3704a6debbc45b149a26bb2d64b18bb1c9e
Sabrina <sabrinalbolognese@gmail.com>
2016-06-04 22:18:38 -0700

    some js changes

--- public/studentScript.js
+++ public/studentScript.js
@@ -6,3 +6,36 @@ function displayFav(fav,id, showing){
     document.getElementById(id).innerHTML = " ";
   }
 }
+
+function highlightLow(){
+  var badStudents = document.getElementsByClassName("true");
+  if (badStudents.length !=0) {
+    for (i=0;i<badStudents.length;i++){
+      badStudents[i].style.border =" 1px solid red";
+    }
+  }
+}
+
+function highlightHi(){
+  var goodStudents = document.getElementsByClassName("false");
+  if (goodStudents.length !=0) {
+    for (i=0;i<goodStudents.length;i++){
+      goodStudents[i].style.border =" 1px solid green";
+    }
+  }
+}
+
+function clearMe(){
+  var badStudents = document.getElementsByClassName("true");
+  var goodStudents = document.getElementsByClassName("false");
+  if (goodStudents.length !=0) {
+    for (i=0;i<goodStudents.length;i++){
+      goodStudents[i].style.border ="none";
+    }
+  }
+  if (badStudents.length !=0) {
+    for (i=0;i<badStudents.length;i++){
+      badStudents[i].style.border ="none";
+    }
+  }
+}
